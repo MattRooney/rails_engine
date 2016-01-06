bef2093f646e9a7f17c9421df531d3310bcca792
Matt Rooney <matthewjrooney@gmail.com>
2016-01-05 19:40:33 -0700

    remove cc expiration from transactions - passes three additional tests in the spec harness

--- db/schema.rb
+++ db/schema.rb
@@ -66,10 +66,9 @@ ActiveRecord::Schema.define(version: 20160105204953) do
   create_table "transactions", force: :cascade do |t|
     t.integer  "invoice_id"
     t.string   "credit_card_number"
-    t.string   "credit_card_expiration_date"
     t.string   "result"
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
   end
 
   add_index "transactions", ["invoice_id"], name: "index_transactions_on_invoice_id", using: :btree
